e0bacb0fc613619d137d118f4023d598c829d32b
mici-ym <mrdky1@hotmail.com>
2024-02-11 21:39:39 +0200

    docjs

--- import/check/check-photos.js
+++ import/check/check-photos.js
@@ -22,11 +22,23 @@ async function checkPhotos(arrPhoto, wikitext) {
   }
   return objPhotos;
 }
-
+/**
+ * Asynchronously checks if a photo exists in the database.
+ *
+ * @param {string} photo - The photo to be checked.
+ * @returns {Promise<string>} - A promise that resolves to either the netfree image or an error message.
+ */
 async function checkPhoto(photo) {
   const res = await clientPhotos.search(photo);
   return res.image.netfree || res.error.message;
 }
+/**
+ * Removes a photo from the given text.
+ *
+ * @param {string} photo - The photo to be removed.
+ * @param {string} text - The text from which the photo should be removed.
+ * @returns {string} - The updated text with the photo removed.
+ */
 function removePhoto(photo, text) {
   const regex = new RegExp(`[[${photo}]]`, "g");
   return text.replace(regex, "");
