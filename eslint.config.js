90c4d88f3b66f4a14854d4b145574f367f36099c
mici-ym <mrdky1@hotmail.com>
2024-09-12 18:11:58 +0300

    fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default {
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignores: [
+    "node_modules/*",
+    "**/wiki/*",
+    "**/check-wiki-event",
+    "**/import/*",
+  ],
+};
--- src/import/check/filter.js
+++ src/import/check/filter.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import lists from "./listes.json" with {type: 'json'};
 
 /**
