5cba8888e968b243b7b6ef51e028b4ce2bdb88c5
mici-ym <mrdky1@hotmail.com>
2024-11-21 18:25:20 +0200

    add random code for not error in eslint

--- src/import/check/check-wiki-event.js
+++ src/import/check/check-wiki-event.js
@@ -1,7 +1,10 @@
 import { request } from "../../requests/requests.js";
 
-const request = new request("https://he.wikipedia.org/w/api.php");
+const wikipediaApi = new request("https://he.wikipedia.org/w/api.php");
 
 async function checkEvent(title) {
-    
+  wikipediaApi.get("title").then(function (response) {
+    console.log(response);
+  });
 }
+checkEvent("abc");
