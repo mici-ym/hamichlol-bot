c21cc92143babf918d5da22cb25974b594126532
mici-ym <mrdky1@hotmail.com>
2024-12-11 11:10:45 +0200

    edit params

--- src/scriptes/logMoveOfWiki.js
+++ src/scriptes/logMoveOfWiki.js
@@ -1,6 +1,7 @@
 import { requests } from "../requests/requests.js";
 
-const wikiRequest = new requests("https://import.hamichlol.org.il/");
+const wikiRequest = new requests("https://he.wikipedia.org/w/api.php");
+wikiRequest.withLogedIn = false;
 const request = new requests("https://www.hamichlol.org.il/w/api.php");
 
 /**
@@ -16,12 +17,11 @@ async function main(ns = 0) {
       list: "logevents", // Specifies the type of list to fetch - log events.
       leprop: "title|type|timestamp|comment|details", // Properties to include in each log event.
       letype: "move", // Filters the log events to only include "move" type events.
-      lestart: "2024-01-01T00:00:00.000Z", // The start timestamp for fetching log events.
-      leend: "2019-01-00T00:00:00.000Z", // The end timestamp for fetching log events.
+      lestart: "2024-12-01T00:00:00.000Z", // The start timestamp for fetching log events.
+      leend: "2024-10-00T00:00:00.000Z", // The end timestamp for fetching log events.
       lenamespace: ns, // Specifies the namespace of the pages to fetch log events for.
       lelimit: "max", // Sets the limit for the number of log events to fetch to the maximum allowed.
     },
-    withCookie: false, // Indicates whether to include cookies in the request.
   });
   processor(res, ns); // Passes the fetched log events to the processor function for processing.
   console.log(res.length); // Logs the length of the response to the console.
@@ -32,7 +32,7 @@ async function main(ns = 0) {
  * This function determines the appropriate request object to use based on the target parameter,
  * then queries for pages information including redirects. It supports querying from either
  * a local or external wiki API, as indicated by the `target` parameter.
- * 
+ *
  * @param {string[]} titles - An array of page titles to query information for.
  * @param {string} target - A string indicating the target API to use. Can be 'wiki' for the external wiki API or any other string for the local API.
  * @returns {Promise<Object>} A promise that resolves to an object containing the queried pages' data.
@@ -90,8 +90,8 @@ async function processor(list, ns) {
   const stringOfNs = {
     0: "ערכים",
     10: "תבניות",
-    14: "קטגוריות"
-  }
+    14: "קטגוריות",
+  };
   const logList = processorLog(list);
   const dataForPagesLocal = await createTable(
     logList.flatMap((obj) => [obj.from, obj.to]),
@@ -110,38 +110,48 @@ async function processor(list, ns) {
       (cackDataPage(dataForPagesLocal[item.from]) ===
         cackDataPage(dataForPagesWiki[item.from]) &&
         cackDataPage(dataForPagesLocal[item.to]) === "exist") ||
-      (/(BDSM|להט"ב|לט"ב|להטב"ק|לסבית|לסביות|סקסואל|קסואל|מצעד הגאווה|מיניות|פורנוגרפיה|\[\[פין\]\]|\[\[פות\]\])/.test(item.to) ||
-        /(BDSM|להט"ב|לט"ב|להטב"ק|לסבית|לסביות|סקסואל|קסואל|מצעד הגאווה|מיניות|פורנוגרפיה|\[\[פין\]\]|\[\[פות\]\])/.test(item.from))
+      /(BDSM|להט"ב|לט"ב|להטב"ק|לסבית|לסביות|סקסואל|קסואל|מצעד הגאווה|מיניות|פורנוגרפיה|\[\[פין\]\]|\[\[פות\]\])/.test(
+        item.to
+      ) ||
+      /(BDSM|להט"ב|לט"ב|להטב"ק|לסבית|לסביות|סקסואל|קסואל|מצעד הגאווה|מיניות|פורנוגרפיה|\[\[פין\]\]|\[\[פות\]\])/.test(
+        item.from
+      )
     ) {
       continue;
     } else {
-      stringData += `* [[${item.from}]] <small>(מ: ${objOfHe[cackDataPage(dataForPagesLocal[item.from])]
-        }, w: ${objOfHe[cackDataPage(dataForPagesWiki[item.from])]})</small> => [[${item.to}]] <small>(${objOfHe[cackDataPage(dataForPagesLocal[item.to])]
-        })</small>\n`;
+      stringData += `* [[${item.from}]] <small>(מ: ${
+        objOfHe[cackDataPage(dataForPagesLocal[item.from])]
+      }, w: ${
+        objOfHe[cackDataPage(dataForPagesWiki[item.from])]
+      })</small> => [[${item.to}]] <small>(${
+        objOfHe[cackDataPage(dataForPagesLocal[item.to])]
+      })</small>\n`;
     }
   }
-  request.edit({
-    title: "משתמש:מוטי בוט/לוגים",
-    text: `{{טורים|תוכן=\n${stringData}}}\n@[[משתמש:מוטי|מוטי]] ~~~~`,
-    summary: 'דו"ח העברות ויקי',
-    section: "new",
-    sectiontitle: `דו"ח העברות ויקי - ${stringOfNs[ns]}`,
-    nocreate: false,
-  }).finally((data) => {
-    console.log(data)
-    switch (ns) {
-      case 0:
-        main(14)
-        break;
-      case 14:
-        main(10)
-        break;
+  request
+    .edit({
+      title: "משתמש:מוטי בוט/לוגים",
+      text: `{{טורים|תוכן=\n${stringData}}}\n@[[משתמש:מוטי|מוטי]] ~~~~`,
+      summary: 'דו"ח העברות ויקי',
+      section: "new",
+      sectiontitle: `דו"ח העברות ויקי - ${stringOfNs[ns]}`,
+      nocreate: false,
+    })
+    .finally((data) => {
+      console.log(data);
+      switch (ns) {
+        case 0:
+          main(14);
+          break;
+        case 14:
+          main(10);
+          break;
 
-      default:
-        console.log("done");
-        break;
-    }
-  })
+        default:
+          console.log("done");
+          break;
+      }
+    });
 }
 
 function cackDataPage(data) {
