597492450088e3c11961c6e22c696d3591e0576d
mici=ym <mordechy@outlook.co.il>
2024-12-18 13:13:46 +0200

    test

---
+++ src/scripts/testMaxStringLength.js
@@ -0,0 +1,33 @@
+import { Requests } from "../requests/requests.js";
+
+async function testMaxStringLength() {
+  const wikiRequest = new Requests("https://he.wikipedia.org/w/api.php");
+  wikiRequest.withLogedIn = false;
+
+  let testString = "a";
+  let maxLength = 0;
+  const increment = 100; // מספר התווים שיש להוסיף בכל צעד
+
+  while (true) {
+    testString += "a".repeat(increment);
+    try {
+      const response = await wikiRequest.query({
+        options: {
+          list: "test", // שנה את הפרמטרים לפי הצורך
+          prop: "info",
+          titles: testString,
+          // ניתן להוסיף פרמטרים נוספים כאן
+        },
+      });
+      console.log(`ניסיון עם אורך מחרוזת ${testString.length} הצליח.`);
+      maxLength = testString.length;
+    } catch (error) {
+      console.log(`ניסיון עם אורך מחרוזת ${testString.length} נכשל.`);
+      break;
+    }
+  }
+
+  console.log(`אורך המחרוזת המקסימלי שהשרת מקבל: ${maxLength}`);
+}
+
+testMaxStringLength();
