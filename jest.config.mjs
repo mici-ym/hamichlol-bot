3bf42f0d91a53442873e8e8ff4c25205b2099edb
michi <mordechy@outlook.co.il>
2025-08-26 20:21:43 +0300

    update Dependencies

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,9 @@ export default {
     '^.+\\.jsx?$': 'babel-jest',
   },
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.js'],
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(node-fetch|fetch-blob|data-uri-to-buffer|formdata-polyfill)/)'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
 };
