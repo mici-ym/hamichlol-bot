1cc63ae482fa6a64f6913c412301586816542cd0
michi <mordechy@outlook.co.il>
2025-08-26 19:38:47 +0300

    feat: add globals package and update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 //import babelParser from "@babel/eslint-parser";
 //import babelPluginSyntaxImportAssertions from "@babel/plugin-syntax-import-assertions";
+import globals from "globals";
 
 export default {
   languageOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",/*
+    sourceType: "module",
+    globals: {
+      ...globals.node,
+    },/*
     parser: babelParser,
     parserOptions: {
       requireConfigFile: false,
